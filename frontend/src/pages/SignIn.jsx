a075ec53f30e6b790d21b23c2f55876321890177
Blxee <aminedev57@gmail.com>
2024-03-19 17:43:33 +0000

    Added: basic implementaion for sign-in in front-end side.

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,5 +1,29 @@
 export default function SignIn() {
+  const submitForm = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Array.from(formData.entries()).map(([key, value]) => ({ [key]: value }));
+    console.log(data);
+
+    fetch('/api/v1/sign-in', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        console.log('Signed in successfully!');
+        window.location.pathname = '/'; // redirect to homepage
+      }
+    })
+  };
+
   return (
-    <h1>Sign In to your account!</h1>
+    <form onSubmit={submitForm}>
+      <legend>Sign In to your account!</legend>
+      <input name='email' type='email' placeholder='Email' required />
+      <input name='password' type='password' placeholder='Password' required />
+      <input type='submit' />
+    </form>
   )
 }
