9146f8eff107be504c2ec785927bd67eea216090
Blxee <aminedev57@gmail.com>
2024-03-22 04:11:30 +0000

    Renamed: schema.sql to schema_dev.sql to separate dev and prod

--- schema_dev.sql
+++ schema_dev.sql
@@ -1,3 +1,3 @@
 CREATE USER IF NOT EXISTS 'mm_user'@'localhost' IDENTIFIED BY 'good-password';
-CREATE DATABASE IF NOT EXISTS membermaster_dev;
+CREATE DATABASE IF NOT EXISTS membermaster_dev DEFAULT CHARACTER SET 'utf8';
 GRANT ALL PRIVILEGES ON membermaster_dev.* TO 'mm_user'@'localhost';
