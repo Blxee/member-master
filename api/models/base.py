377f974595e79c431e8ef439ba9571cf337619cd
Blxee <aminedev57@gmail.com>
2024-03-22 03:44:53 +0000

    Added: save and delete methods to the Base class

--- api/models/base.py
+++ api/models/base.py
@@ -1,20 +1,37 @@
 """Module for Base class."""
-import my
+from flask import current_app
+from datetime import datetime
 
 
 class Base:
     """Base model handles CRUD operation on the database."""
-    def __init__(self, **kwargs) -> None:
-        self.id = None
-        self.date_created = None
-        self.date_updated = None
-        pass
+
+    table_name: str
+    id: int
+    date_created: datetime
+    date_updated: datetime
+
+    def __init__(self) -> None:
+        raise NotImplementedError('the Base class is abstract!')
 
     def save(self):
-        pass
+        """Saves the current instance to the database."""
+        cursor = current_app.mysql_client.cursor()
+        cursor.execute(
+            f"""REPLACE INTO ${self.table_name} WHERE id = '%s'""",
+            (self.id,),
+        )
+        cursor.close()
+
 
     def delete(self):
-        pass
+        """Deletes the current instance from the database."""
+        cursor = current_app.mysql_client.cursor()
+        cursor.execute(
+            f"""DELETE FROM ${self.table_name} WHERE id = '%s'""",
+            (self.id,),
+        )
+        cursor.close()
 
     @classmethod
     def all(cls):
@@ -27,3 +44,8 @@ class Base:
     @classmethod
     def search(cls, **kwargs):
         pass
+
+    @classmethod
+    def __init_subclass__(cls):
+        '''Sets the class attr table_name to be the class name in lowercase.'''
+        cls.table_name = cls.__name__.lower()
