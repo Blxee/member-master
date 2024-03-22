8b40fdedb912ed3d6724661bd36d6f1a0b5152aa
Blxee <aminedev57@gmail.com>
2024-03-22 15:59:23 +0000

    Fixed: perfected the react routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,21 +10,25 @@ import SignIn from './pages/SignIn';
 import Nav from './Nav';
 import Footer from './Footer';
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/sign-up', element: <SignUp /> },
-  { path: '/sign-in', element: <SignIn /> },
-])
+const router = createBrowserRouter([{
+  path: '/',
+  element: (
+    <div id='main-container' className='d-flex flex-column'>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  ),
+  children: [
+    { path: '', element: <Home /> },
+    { path: 'sign-up', element: <SignUp /> },
+    { path: 'sign-in', element: <SignIn /> },
+  ]
+}]);
 
 function App() {
 
-  return (
-    <div id='main-container' className='d-flex flex-column'>
-      <Nav />
-      <RouterProvider router={router} />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} fallbackElement={<h1>loading</h1>} />;
 }
 
 export default App
--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { useRef } from "react"
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   const navRef = useRef();
@@ -11,11 +12,20 @@ export default function Nav() {
   return (
     <>
       <div style={{height}} />
-      <nav ref={navRef} className='position-fixed' style={{float: 'left'}}>
+      <nav ref={navRef} className='position-fixed'>
         <span>MemberMaster</span>
-        <button>Home</button>
-        <button>Dashboard</button>
-        <button>Plan</button>
+
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+
+        <Link to='/sign-in'>
+          <button>Sign In</button>
+        </Link>
+
+        <Link to='/sign-up'>
+          <button>Sign Up</button>
+        </Link>
       </nav>
     </>
   )
