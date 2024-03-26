a60a8facccdb4f781e045f4124bf289892c47266
Blxee <aminedev57@gmail.com>
2024-03-26 01:28:10 +0000

    Updated: schema_dev.sql to add businesses and different relationships

--- schema_dev.sql
+++ schema_dev.sql
@@ -9,4 +9,21 @@ CREATE TABLE IF NOT EXISTS membermaster_dev.users (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS membermaster_dev.businesses (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  logo VARCHAR(256),
+  owner_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(owner_id) REFERENCES membermaster_dev.users(id)
+);
+
+CREATE TABLE IF NOT EXISTS membermaster_dev.business_clients (
+  client_id INT,
+  business_id INT,
+  PRIMARY KEY(client_id, business_id),
+  FOREIGN KEY(client_id) REFERENCES membermaster_dev.users(id),
+  FOREIGN KEY(business_id) REFERENCES membermaster_dev.businesses(id)
+);
+
 GRANT ALL PRIVILEGES ON membermaster_dev.* TO 'mm_user_dev'@'%';
