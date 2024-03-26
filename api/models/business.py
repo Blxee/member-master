b059df05660fe2ba936b2e38dfac78e71b676093
Blxee <aminedev57@gmail.com>
2024-03-26 16:34:11 +0000

    Created: Business model with get_clients() function

---
+++ api/models/business.py
@@ -0,0 +1,35 @@
+"""Module for the Business class defintion."""
+from flask import current_app
+from api.models.base import Base
+from api.models.user import User
+
+
+class Business(Base):
+    """Business class represents a business owned by a User"""
+    table_name = 'businesses'
+    fields = (*Base.fields, 'name', 'logo', 'owner_id') # == (id, name, logo, owner_id)
+
+    def get_clients(self) -> list[User]:
+        """Get all clients subscribed to this business."""
+        cursor = current_app.mysql_client.cursor()
+        cursor.execute(
+            f"""
+            SELECT users.*        FROM users
+            JOIN business_clients ON users.id = business_clients.client_id
+            JOIN businesses       ON business_clients.business_id = businesses.id
+            WHERE businesses.id = %s
+            """,
+            (self.id,))
+
+        users = []
+        col_names, *_ = zip(*cursor.description)
+        for row in cursor.fetchall():
+            kwargs = {
+                col_names[i]: row[i]
+                for i in range(len(col_names))
+            }
+            instance = User(**kwargs)
+            users.append(instance)
+
+        cursor.close()
+        return users
