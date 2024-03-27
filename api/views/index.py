cbedcc5611996cd85559fb1d63bd3bbbbaa165a6
Blxee <aminedev57@gmail.com>
2024-03-27 06:10:24 +0000

    Added: a status route to check on th API

--- api/views/index.py
+++ api/views/index.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, jsonify
 
 
 index_routes = Blueprint('index', __name__, url_prefix='/api')
+
+
+@index_routes.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({'status': 'success', 'message': 'server is running'})
 
 
 @index_routes.route('/', methods=['GET'], strict_slashes=False)
