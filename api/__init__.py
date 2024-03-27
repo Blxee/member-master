99bf8836e4f580a31d7d000490653f36231c691d
Blxee <aminedev57@gmail.com>
2024-03-27 09:30:29 +0000

    Fixed: require_auth decorator name clashing (by using functools.wraps)

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from api.views.users import users_routes
 from api.views.index import index_routes
+from api.views.businesses import businesses_routes
 from api.models.auth.auth import Auth
 from api.models.auth.basic_auth import BasicAuth
 from api.models.auth.token_auth import TokenAuth
@@ -33,5 +34,6 @@ def create_app(test_config=None):
 
     app.register_blueprint(users_routes)
     app.register_blueprint(index_routes)
+    app.register_blueprint(businesses_routes)
 
     return app
--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,11 @@
 """Module for various helpful utils."""
 from flask import current_app, jsonify, request
+from functools import wraps
 
 
 def require_auth(fun):
     """Used to decorate routes which require authentication."""
+    @wraps(fun)
     def inner(*args, **kwargs):
         auth_user = current_app.auth.current_user(request)
         if auth_user is None:
