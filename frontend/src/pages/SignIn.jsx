5cb3d350e87dc0fe32f8a41cae30488507cdf7fd
Blxee <aminedev57@gmail.com>
2024-03-27 06:25:06 +0000

    Updated: login page frontend (styling is now way better)

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SignIn() {
+  const navigate = useNavigate();
+
   const submitForm = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -7,23 +11,28 @@ export default function SignIn() {
 
     fetch('/api/v1/sign-in', {
       method: 'POST',
-      credentials: 'same-origin',
+      credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     }).then((res) => {
       if (res.ok) {
         console.log('Signed in successfully!');
-        window.location.pathname = '/'; // redirect to homepage
+        navigate('/')
       }
     })
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={submitForm} className="card container p-4 gap-3 mt-5">
       <legend>Sign In to your account!</legend>
-      <input name='email' type='email' placeholder='Email' required />
-      <input name='password' type='password' placeholder='Password' required />
-      <input type='submit' />
+
+      <label className="form-label">Email Address:</label>
+      <input className="form-control" name='email' type='email' placeholder='Email' required />
+
+      <label className="form-label">Password</label>
+      <input className="form-control" name='password' type='password' placeholder='Password' required />
+
+      <input className="btn btn-primary bg-gradient w-25 align-self-center" type='submit' />
     </form>
   )
 }
