bdb40007ef09c9a88394fbbab1e62c6d4b126a57
Blxee <aminedev57@gmail.com>
2024-04-04 00:45:31 +0000

    Fixed: syntax error in schema_dev.sql

--- schema_dev.sql
+++ schema_dev.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS membermaster_dev.businesses (
 -- subscription: many to many relationship between a client and a business
 CREATE TABLE IF NOT EXISTS membermaster_dev.subscriptions (
   business_id INT NOT NULL,
-  client_id INT NOT NULL AUTO_INCREMENT, --this is only for quick deployment
+  client_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(64),
   last_name VARCHAR(64),
   picture VARCHAR(128),
@@ -38,8 +38,8 @@ CREATE TABLE IF NOT EXISTS membermaster_dev.subscriptions (
   last_paid DATE,
   assurance BOOLEAN,
   PRIMARY KEY(business_id, client_id),
-  FOREIGN KEY(business_id) REFERENCES membermaster_dev.businesses(id) ON DELETE CASCADE
-  FOREIGN KEY(client_id) REFERENCES membermaster_dev.users(id) ON DELETE CASCADE,
+  FOREIGN KEY(business_id) REFERENCES membermaster_dev.businesses(id) ON DELETE CASCADE,
+  FOREIGN KEY(client_id) REFERENCES membermaster_dev.users(id) ON DELETE CASCADE
 );
 
 GRANT ALL PRIVILEGES ON membermaster_dev.* TO 'mm_user_dev'@'%';
